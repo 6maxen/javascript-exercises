1b8767c5a9ebe414ae36202f62d847fa018977d1
max1 <240841057+6maxen@users.noreply.github.com>
2025-10-29 19:47:58 -0400

    Complete 1-7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    temp = string.split('')
+    temp.reverse()
+    result = temp.join('')
+    return result
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...value) {
+    for (i=0; i < value.length; i++) {
+        array.splice(value[i])
+    }
+    return array
+
 };
 
 // Do not edit below this line
